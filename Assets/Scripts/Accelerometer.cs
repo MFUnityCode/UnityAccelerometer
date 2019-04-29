e7dc980a6ca5c8a84a899d061d2272f91cd23e0d
Aluno <aluno@naverecife.local>
2019-04-29 15:31:15 -0300

    NoReverseEngine

--- Assets/Scripts/Accelerometer.cs
+++ Assets/Scripts/Accelerometer.cs
@@ -6,39 +6,18 @@ using UnityEngine.UI;
 public class Accelerometer : MonoBehaviour
 {
 
-    [Header("Primary Variables")]
-    [SerializeField] private float accelerometerUpdateInterval = 1.0f / 60.0f;
-    // The greater the value of LowPassKernelWidthInSeconds, the slower the
-    // filtered value will converge towards current input sample (and vice versa).
-    [SerializeField] private float lowPassKernelWidthInSeconds = 1.0f;
-    // This next parameter is initialized to 2.0 per Apple's recommendation,
-    // or at least according to Brady! ;)
-    [SerializeField] private float shakeDetectionThreshold = 2.0f;
-    [SerializeField] private float lowPassFilterFactor;
-    [SerializeField] private Vector3 lowPassValue;
-
     [Header("References")]
     [SerializeField] private Text text;
+    [SerializeField] private Text textInfo;
 
     void Start()
     {
-        lowPassFilterFactor = accelerometerUpdateInterval / lowPassKernelWidthInSeconds;
-        shakeDetectionThreshold *= shakeDetectionThreshold;
-        lowPassValue = Input.acceleration;
+        
     }
 
     void Update()
     {
-        Vector3 acceleration = Input.acceleration;
-        lowPassValue = Vector3.Lerp(lowPassValue, acceleration, lowPassFilterFactor);
-        Vector3 deltaAcceleration = acceleration - lowPassValue;
-
-        if (deltaAcceleration.sqrMagnitude >= shakeDetectionThreshold)
-        {
-            // Perform your "shaking actions" here. If necessary, add suitable
-            // guards in the if check above to avoid redundant handling during
-            // the same shake (e.g. a minimum refractory period).
-            Debug.Log("Shake event detected at time "+Time.time);
-        }
+        Vector3 ac = Input.acceleration;
+        textInfo.text = "X = " + ac.x + "Y = " + ac.y;
     }
 }
